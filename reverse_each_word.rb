7472ae8900d2f68bfc08f30ec00619dcc64c358d
Daniel Calvin <arockne2003@yahoo.com>
2021-09-29 10:34:45 -0700

    Implement revere_each_word

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,10 @@
-# Write your code here
+require 'pry'
+#split the string between spaces
+#reverse each word add it to a new array
+#join the array of words on a space
+#return the reversed sentence
+def reverse_each_word(sentence)
+  sentence.split(' ').map(&:reverse).join(' ')
+end
+
+puts reverse_each_word("Hello there, and how are you?")
